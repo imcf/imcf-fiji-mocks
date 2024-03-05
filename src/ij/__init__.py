220e7a9bb5c0650b462d22a3765bcf665280eb06
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2024-03-05 16:32:39 +0100

    Improve ij.IJ mock to provide an IJ.run() method

--- src/ij/__init__.py
+++ src/ij/__init__.py
@@ -1,4 +1,23 @@
-IJ = None
+from __future__ import print_function
+
+import logging
+
 ImageStack = None
 Prefs = None
 ImagePlus = None
+
+
+_log = logging.getLogger()
+_log.setLevel(logging.DEBUG)
+
+
+class IJ(object):
+
+    """Dummy class providing a way to call `IJ.run()`.
+
+    The sole purpose of this is to be used with pytest by simply printing the
+    command and its parameters to stdout."""
+
+    @staticmethod
+    def run(cmd, params):
+        _log.warning("IJ.run(cmd=[%s], params=[%s])", cmd, params)
