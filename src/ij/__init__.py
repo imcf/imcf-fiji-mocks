2811a1234ab8387eaee5f86685cd23a019a622bb
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2026-01-16 15:52:19 +0100

    Fix docstring

--- src/ij/__init__.py
+++ src/ij/__init__.py
@@ -12,12 +12,12 @@ _log.setLevel(logging.DEBUG)
 
 
 class IJ(object):
-
     """Dummy class providing a way to call `IJ.run()`.
 
     The sole purpose of this is to be used with pytest by simply issuing a log
     message containing the requested command and its parameters in a
-    well-defined format (so it can be )."""
+    well-defined format (so it can be ).
+    """
 
     @staticmethod
     def run(cmd, params):
